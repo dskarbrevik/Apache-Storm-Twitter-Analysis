e709b05860be76be68562ef2acf3b68e0047172b
David Skarbrevik <skarbrevik@ischool.berkeley.edu>
2016-11-20 17:51:27 -0800

    All files for Exercise 2 except architecture.pdf

---
+++ tweetwordcount.clj
@@ -0,0 +1,34 @@
+(ns tweetwordcount
+  (:use     [streamparse.specs])
+  (:gen-class))
+
+(defn tweetwordcount [options]
+   [
+    ;; spout configuration
+    {"tweet-spout" (python-spout-spec
+          options
+          "spouts.tweets.Tweets"
+          ["tweet"]
+          :p 1
+          )
+    }
+    ;; bolt configuration 1
+    {"parse-tweet-bolt" (python-bolt-spec
+          options
+          {"tweet-spout" :shuffle}
+          "bolts.parse.ParseTweet"
+          ["word"]
+          :p 2
+          )
+
+     ;; bolt configuration 2
+     "count-bolt" (python-bolt-spec
+          options
+          {"parse-tweet-bolt" ["word"]}
+          "bolts.wordcount.WordCounter"
+          ["word" "count"]
+          :p 2
+          )
+    }
+  ]
+)
